51034c54459eadc8ceb39d22520285de3a57e334
DidasMba <daniel.mbarushi@gmail.com>
2023-10-27 16:39:37 +0300

    recap

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+
+
+//export default App;
+
+import GalleryG from './recap';
+
+export default function App(){
+
+    return(
+         <GalleryG />
+
+    
+    );
+ 
+  }
---
+++ src/recap.js
@@ -0,0 +1,19 @@
+function Profile(){
+    return(
+        <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Didas"
+      />
+    );
+}
+
+export default function GalleryG() {
+    return (
+      <section>
+        <h1>Didas : Frontend developer</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
